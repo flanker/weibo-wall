e636002c82f74063f4fbdbfff8b24e2532707812
Zation <zation1@gmail.com>
2012-05-29 23:09:01 +0800

    finish the draft style

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,23 @@ $(function() {
   var directive = {
     'li': {
       'feed<-feeds': {
+        '.name': 'feed.name',
         '.context': 'feed.context',
-        'img@src': 'feed.pic'
+        '.avatar@src': 'feed.pic'
       }
     }
   };
 
   $.getJSON('/topic.json', function(data) {
-    $('.feeds').render(data, directive);
+    data['feeds'] = data['feeds'].slice(0, 3);
+    var context, name, seperatorIndex, feeds = data['feeds'];
+    for (var index in feeds) {
+      context = feeds[index]['context'];
+      seperatorIndex = context.indexOf('&#65306;');
+      feeds[index]['name'] = context.substring(0, seperatorIndex);
+      feeds[index]['context'] = context.substring(seperatorIndex + 8);
+    }
+
+    $('.wall').render(data, directive);
   });
 });

4a455f88bed55c84dfd7f9b142260a187a2a2af4
Zation <zation1@gmail.com>
2012-05-29 18:33:21 +0800

    add compass

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+$(function() {
+  var directive = {
+    'li': {
+      'feed<-feeds': {
+        '.context': 'feed.context',
+        'img@src': 'feed.pic'
+      }
+    }
+  };
+
+  $.getJSON('/topic.json', function(data) {
+    $('.feeds').render(data, directive);
+  });
+});
